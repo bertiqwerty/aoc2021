21b62bd3230d06d9020ead245aee161d68ee3dcd
Behrang Shafei <behrang.shafei@basf.com>
2021-12-04 22:30:27 +0100

    deinit

--- src/helpers.zig
+++ src/helpers.zig
@@ -7,16 +7,16 @@ pub const Task = enum { first, second };
 pub fn read_file_to_string_array(fname:[]const u8) !ArrayListOfStr {
     var file = try std.fs.cwd().openFile(fname, .{});
     defer file.close();
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-
-    var res = ArrayListOfStr.init(&gpa.allocator);
+    
+    var res = ArrayListOfStr.init(std.heap.page_allocator);
     var buf_reader = std.io.bufferedReader(file.reader());
     var in_stream = buf_reader.reader();
 
     var buf: [str_len]u8 = undefined;
-    
+    var gpa_inner = std.heap.GeneralPurposeAllocator(.{}){};
+        
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        var s = std.ArrayList(u8).init(&gpa.allocator);
+        var s = std.ArrayList(u8).init(&gpa_inner.allocator);
         try s.appendSlice(line);
         try res.append(s);
     }
--- src/main.zig
+++ src/main.zig
@@ -4,8 +4,8 @@ const day1 = @import("day1.zig");
 const day2 = @import("day2.zig");
 
 fn to_ints(input: hlp.ArrayListOfStr) !std.ArrayList(i32) {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    var res = std.ArrayList(i32).init(&gpa.allocator);
+    // var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var res = std.ArrayList(i32).init(std.testing.allocator);
     for (input.items) |s| {
         const parsed = try std.fmt.parseInt(i32, s.items, 10);
         try res.append(parsed);
@@ -15,22 +15,30 @@ fn to_ints(input: hlp.ArrayListOfStr) !std.ArrayList(i32) {
 
 pub fn main() anyerror!void {
     std.log.info("AOC 2021.", .{});
-    const day1_in = try to_ints(try hlp.read_file_to_string_array("input_data/day1.txt"));
-    const resd011 = day1.run(day1_in.items, hlp.Task.first);
-    std.log.info("day1 1 {}", .{resd011});
-    const resd012 = day1.run(day1_in.items, hlp.Task.second);
-    std.log.info("day1 2 {}", .{resd012});
-
-    const day2_in = try hlp.read_file_to_string_array("input_data/day2.txt");
-    const resd021 = day2.run(day2_in, hlp.Task.first);
-    std.log.info("day2 1 {}", .{resd021});
-    const resd022 = day2.run(day2_in, hlp.Task.second);
-    std.log.info("day2 2 {}", .{resd022});
-    
+    {
+        const day1_in = try hlp.read_file_to_string_array("input_data/day1.txt");
+        const day1_in_int = try to_ints(day1_in);
+        defer {
+            day1_in_int.deinit();
+            day1_in.deinit();
+        }
+        const resd011 = day1.run(day1_in_int.items, hlp.Task.first);
+        std.log.info("day1 1 {}", .{resd011});
+        const resd012 = day1.run(day1_in_int.items, hlp.Task.second);
+        std.log.info("day1 2 {}", .{resd012});
+    }
+    {
+        const day2_in = try hlp.read_file_to_string_array("input_data/day2.txt");
+        defer day2_in.deinit();
+        const resd021 = day2.run(day2_in, hlp.Task.first);
+        std.log.info("day2 1 {}", .{resd021});
+        const resd022 = day2.run(day2_in, hlp.Task.second);
+        std.log.info("day2 2 {}", .{resd022});
+    }
 }
 
 test "to ints" {
     const x = try to_ints(try hlp.read_file_to_string_array("input_data/day1.txt"));
     try std.testing.expectEqual(x.items[0], 170);
-    try std.testing.expectEqual(x.items[x.items.len-1], 7181);
+    try std.testing.expectEqual(x.items[x.items.len - 1], 7181);
 }
